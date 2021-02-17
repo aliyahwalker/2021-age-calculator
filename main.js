9ebbfba07b034f3b2638a9b338c03acc7db85efb
Aliyah Walker <aliyahwalker@Aliyahs-MacBook-Pro.local>
2021-02-16 20:44:03 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function age_calculator(family_member, name, age) {
+	return `My ${family_member}, ${name}, is ${age} years old (${age * 12} months)`;
+}
+age_calculator("father", "Vernon", 60);
+age_calculator("mother", "Sabrina", 50);
+age_calculator("sister", "Aminah", 28);
